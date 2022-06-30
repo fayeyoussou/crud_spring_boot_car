34f991e984fa6b11a2f355df32064cb1ddff743c
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2022-06-30 20:41:29 +0000

    adding other thing

--- src/main/java/sn/youdev/restcarproject/RestCarProjectApplication.java
+++ src/main/java/sn/youdev/restcarproject/RestCarProjectApplication.java
@@ -9,6 +9,8 @@ import sn.youdev.restcarproject.domain.Owner;
 import sn.youdev.restcarproject.repository.CarRepository;
 import sn.youdev.restcarproject.repository.OwnerRepository;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class RestCarProjectApplication implements CommandLineRunner {
 
@@ -21,32 +23,45 @@ public class RestCarProjectApplication implements CommandLineRunner {
     private OwnerRepository ownerRepository;
     @Override
     public void run(String... args) throws Exception {
-        Owner owner = new Owner();
-        owner.setFirstName("Youssoupha");
-        owner.setLastName("Faye");
-        ownerRepository.save(owner);
-        Owner owner2 = new Owner();
-        owner2.setFirstName("Tapha");
-        owner2.setLastName("Sow");
-        ownerRepository.save(owner2);
-        Car car = new Car();
-        car.setBrand("Toyota");
-        car.setColor("red");
-        car.setModele("Yaris");
-        car.setPrice(4000);
-        car.setOwner(owner);
-        car.setYear(2019);
-        car.setRegisterNumber("A3495Z");
-        carRepository.save(car);
-        Car car2 = new Car();
-        car2.setBrand("Jeep");
-        car2.setColor("black");
-        car2.setModele("Renegade");
-        car2.setPrice(23000);
-        car2.setOwner(owner2);
-        car2.setYear(2016);
-        car2.setRegisterNumber("X4578S");
-        carRepository.save(car2);
+        Owner owner = new Owner("mohamed","thiam");
+        Owner owner2 = new Owner("ibrahima","Niang");
+        ownerRepository.saveAll(Arrays.asList(owner,owner2));
+        Car car1 = new Car(
+                "Mercedez",
+                "benz",
+                "grey",
+                "A3489T",
+                2004,
+                1009,
+                owner);
+        Car car2 = new Car(
+                "Nissan",
+                "unknown",
+                "night blue",
+                "H4563P",
+                2009,
+                1890,
+                owner);
+        Car car3 = new Car(
+                "Peugeot",
+                "306",
+                "Green",
+                "L5673K",
+                1999,
+                2000,
+                owner2);
+        Car car4 = new Car(
+                "Peugeo",
+                "unknowm",
+                "Pink",
+                "D2789M",
+                2008,
+                3100,
+                owner2);
+        carRepository.saveAll(Arrays.asList(car1,car2,car3,car4));
+
+
+
 
     }
 }
